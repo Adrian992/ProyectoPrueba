285df5f7d039a854cbeec9de2b678781e931eb95
Adrian992 <49647157+Adrian992@users.noreply.github.com>
2020-03-28 14:52:03 -0500

    Add files via upload

---
+++ Frontend/Graduados/src/app/graduado.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GraduadoService {
+
+  private baseURL = 'http://localhost:8080/api/graduado';
+
+  constructor(private http:HttpClient) { }
+
+  createGraduado(graduado:Object):Observable<Object>{
+    return this.http.post(`${this.baseURL}`,graduado);
+  }
+
+  updateGraduado(graduado:Object):Observable<Object>{
+    return this.http.put(`${this.baseURL}`,graduado);
+  }
+
+  deleteGraduado(id:number):Observable<any>{
+    return this.http.delete(`${this.baseURL}/${id}`,{responseType:'text'});
+  }
+
+  getGraduadosList():Observable<any>{
+    return this.http.get(`${this.baseURL}`);
+  }
+
+  getGraduado(id:number):Observable<any>{
+    return this.http.get(`${this.baseURL}/${id}`);
+  }
+  
+}
